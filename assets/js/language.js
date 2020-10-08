aca20da0531bc70d13fc4f933d6d503e86d1ce39
Rahman Çaylak <rahmancaylakk@gmail.com>
2020-10-08 12:53:32 +0300

    Update language.js

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,5 +1,8 @@
-let userLanguage = navigator.language;
+const userLanguage = navigator.language;
 
 if (userLanguage != 'tr') {
     location.href = "https://rahmancaylak.github.io/en";
+}
+else{
+    location.href = "https://rahmancaylak.github.io/";
 }
