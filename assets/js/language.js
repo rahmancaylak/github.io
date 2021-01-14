71f177a008e496afed2261850d6c0dc6c6c9cf08
Rahman Çaylak <rahmancaylakk@gmail.com>
2021-01-14 19:11:48 +0300

    Delete language.js

--- assets/js/language.js
+++
@@ -1,8 +0,0 @@
-const userLanguage = navigator.language;
-
-if (userLanguage != 'tr') {
-    location.href = "https://rahmancaylak.github.io/en";
-}
-else{
-    location.href = "https://rahmancaylak.github.io/";
-}
